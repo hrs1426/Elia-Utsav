41280d01078bac198ad9a38a4aa538e1dc49a9e2
Harsh Raj <harshkumar9472@gmail.com>
2024-12-01 11:47:29 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://ujjwal313.github.io",
-  base: "Elia-Utsav-NGO",
+  site: "https://hrs146.github.io",
+  base: "Elia-Utsav",
 });
